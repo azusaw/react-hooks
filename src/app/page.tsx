ea89f3b123d40e489390b76eadc411554e885ce8
azusaw <azusa0304@gmail.com>
2023-09-15 15:52:41 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import DescriptionCard from "@/components/DescriptionCard";
+
+export default function Home() {
+  return <main className={styles.main}>hoge</main>;
+}
